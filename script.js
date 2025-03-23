0185b8f8d3794cbda0745aa3c9f4780844dacea4
Shahrozirl <shahrozirl@gmail.com>
2025-03-23 20:00:17 +0000

    fixed navigation bar error and added contacts page

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+  function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("open");
+  }
+
 document.querySelectorAll('.nav-links a').forEach(link =>
     link.addEventListener('click', () => {
       document.getElementById("navLinks").classList.remove("open");
