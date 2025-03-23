9a834adbd342011138b9d567d57f01305e31bd21
Shahrozirl <shahrozirl@gmail.com>
2025-03-23 20:45:23 +0000

    remade navigation bar to be simpler

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 function toggleMenu() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("open");
-  
-    // Remove previous listeners to avoid duplicates
-    const links = document.querySelectorAll('.nav-links a');
-    links.forEach(link => {
-      link.removeEventListener('click', closeMenu); // Ensure no duplicates
-      link.addEventListener('click', closeMenu);
-    });
-  }
-  
-  function closeMenu() {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.remove("open");
-  }
-  
+  const menu = document.getElementById("sideNav");
+  menu.classList.toggle("open");
+}
