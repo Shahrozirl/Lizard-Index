0e150a27820a9ac65dff10c413d8fb5c65aeddc6
Shahrozirl <shahrozirl@gmail.com>
2025-03-24 12:30:17 +0000

    added form functionality

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ function toggleMenu() {
   const menu = document.getElementById("sideNav");
   menu.classList.toggle("open");
 }
+
+const serviceID = "service_pvaz9jx";
+const templateID = "TQnTKivGdFVOB_yY0";
+const publicKey = "TQnTKivGdFVOB_yY0";
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm(serviceID, templateID, this, publicKey)
+    .then(() => {
+      alert("Message sent successfully!");
+      this.reset();
+    }, (error) => {
+      console.error("FAILED...", error);
+      alert("There was an error sending your message. Please try again later.");
+    });
+});
